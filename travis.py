5aff869e0afc5bbe8d3aff3513b2d03fb6f76f58
azman0101 <azman0101@gmail.com>
2015-02-24 14:19:57 +0100

    suffix not float

--- travis.py
+++ travis.py
@@ -5,5 +5,8 @@ version = sys.argv[1]
 if version.startswith('http'):
     print(version)
 else:
-    next_version = float(version) + 0.1
+    if version == '1.8a1':
+        next_version = float(1.8) + 0.1
+    else:
+        next_version = float(version) + 0.1
     print('Django>=%s,<%.1f' % (version, next_version))
