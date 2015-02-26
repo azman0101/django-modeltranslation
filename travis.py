742f47648a9818bdbab6e1bc447011efb99050a0
azman0101 <azman0101@gmail.com>
2015-02-26 09:28:34 +0100

    Fix pip install for 1.8a1

--- travis.py
+++ travis.py
@@ -6,7 +6,7 @@ if version.startswith('http'):
     print(version)
 else:
     if version == '1.8a1':
-        next_version = float(1.8) + 0.1
+        print('Django>=%s' % version)
     else:
         next_version = float(version) + 0.1
-    print('Django>=%s,<%.1f' % (version, next_version))
+        print('Django>=%s,<%.1f' % (version, next_version))
